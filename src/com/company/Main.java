adb7b90a064f49f32952dac1f51b8207f7429686
katsyarina789 <katsyarina.fedorova97@gmail.com>
2019-08-26 01:08:14 +0300

    forth commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	int a = 8;
+
+	for (int i = 1; i < 9; i++) a *= 8;
+        System.out.println(a);
+    }
+    }
+
